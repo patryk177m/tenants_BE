5fe688dd1fdc1b0923cea1a7e7c1e9e1d87fef13
patryk177m <patrykmajewski177@gmail.com>
2024-04-18 11:59:28 +0200

    fix comment cosole.log

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const checkLoggedIn = (req, res, next) => {
 };
 
 // const viewsPath = path.join(__dirname, "views");
-console.log(viewsPath);
+// console.log(viewsPath);
 app.set('views', '/opt/render/project/src/views');
 app.set("view engine", "ejs");
 app.use(express.static("./public"));
