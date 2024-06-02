69043126933e596743e206046700b4f0ad074387
patryk177m <patrykmajewski177@gmail.com>
2024-06-02 14:11:38 +0200

    fix get all oreder by desc

--- src/services/cardService.js
+++ src/services/cardService.js
@@ -1,27 +1,26 @@
 import { Card } from "../models/card-model.js";
 
-
 async function getAll() {
-  return Card.findAll();
-};
+  return Card.findAll({
+    order: [["id", "DESC"]],
+  });
+}
 
 async function getItemById(id) {
   try {
-    
     if (!id) {
-      throw new Error('The item with the given id does not exist');
+      throw new Error("The item with the given id does not exist");
     }
 
     const item = await Card.findByPk(id);
 
     if (!item) {
-      throw new Error('Item not found');
+      throw new Error("Item not found");
     }
 
     return item;
-
   } catch (err) {
-    throw new Error('Error item: ' + err.message);
+    throw new Error("Error item: " + err.message);
   }
 }
 
@@ -29,25 +28,25 @@ async function create(item) {
   try {
     return await Card.create(item);
   } catch (err) {
-    throw new Error('Error creating item: ' + err.message);
+    throw new Error("Error creating item: " + err.message);
   }
-};
+}
 
 async function deleteItem(id) {
   try {
     return await Card.destroy({
       where: {
         id,
-      }
+      },
     });
   } catch (err) {
-    throw new Error('Error deleting item: ' + err.message);
+    throw new Error("Error deleting item: " + err.message);
   }
-};
+}
 
 export const cardService = {
   getAll,
   getItemById,
   create,
   deleteItem,
-}
+};
